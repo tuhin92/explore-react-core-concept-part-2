6e426af0168e96f39f37782f58ee75908b299285
tuhin92 <tuhinalmamun71m@gmail.com>
2024-03-11 09:14:58 +0600

    try event handler with different button with different function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import './App.css'
 
 function App() {
+  
+  // normal function for button 1
+  function handleClick(){
+    alert('button clicked');
+  }
+
+  // arrow function for button 2
+  const handleClick2 = () =>{
+    alert('button-2 clicked')
+  }
   return (
     <>
       <h3>React core concepts 2</h3>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick2}>Button 2</button>
+
+      {/* use function directly in button  */}
+      <button onClick={()=>{alert('button-3 clicked')}}>Button 3</button>
     </>
   )
 }
